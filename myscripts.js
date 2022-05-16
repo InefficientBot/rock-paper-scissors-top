8d4a8b54a4e6d013968a309c3d087d82cf0b5d2c
Khalid <Khalid3x5@gmail.com>
2022-05-16 11:32:02 -0500

    Completed the playRound function with player selection hard coded.

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,28 @@
 const choice = ["Rock", "Paper", "Scissor"];
+let playerScore = 0
+let computerScore = 0
 
 function computerPlay() {
     const random = choice[~~(Math.random() * choice.length)];
     return random;
+}
+function playRound (playerSelection, computerSelection) {
+    playerSelection = 'rock';
+    computerSelection = computerPlay().toLowerCase();
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie game!");
+    }
+    else if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock")
+    ) 
+    {
+        console.log("Oh No! You've Lost!");
+        computerScore++
+    }
+    else {
+        console.log("Great Job! You've Won!")
+        playerScore++
+    }
 }
